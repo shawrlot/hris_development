12fc84e82c6fd42f2b9a3b772475342d130960c3
alohajaycee <alohajaycee@gmail.com>
2017-11-01 22:08:15 +0800

    evaluation view

--- application/views/evaluate_view.php
+++ application/views/evaluate_view.php
@@ -1,7 +1,27 @@
 <?php include 'header.php' ?>
 
 
-
+<div class="BeStart">
+	<div class="container-fluid">
+		<div class="l-center2">
+			<div class="sbefore-card shadow col-sm-4 col-md-6 col-lg-3">
+				<h3 class="accent pb-2 ">Before we start</h3>
+				<hr class="pb-2 border-dark">
+				<form>
+					<div class="form-group">
+						<input type="text" class="form-control" placeholder="Enter your pin">
+					</div>
+					<a href="#">Don't have a pin?</a>
+					<button class="mt-3 btn btn-accent w-100">Verify PIN</button>
+				</form>
+			</div>
+			<div class="text-center pt-5">
+				<a href="#" class="evaluate-l text-white">Here to login? </a>
+				<h5 class="pt-4 ml-3"><a href="" class="evaluate-l text-white">Go to login<i class="material-icons align-middle">keyboard_arrow_right</i> </a></h5>
+			</div>
+		</div>
+	</div>
+</div>
 
 
 
