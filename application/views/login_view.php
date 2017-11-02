c75ff05bcb4e15bca3e662d193cfcc91384adb79
kennethvargas23 <vargaskenneth416@yahoo.com>
2017-11-02 15:30:47 +0800

    changed hello to welcome

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -3,7 +3,7 @@
 	<div class="container-fluid">
 		<div class="l-center2">
 			<div class="login-card shadow col-sm-6 col-md-6 col-lg-3">
-				<h1 class="accent pb-3">Hello</h1>
+				<h1 class="accent pb-3">Welcome</h1>
 				<hr class="pb-4">
 				<form method=POST action="/login">
 					<div class="form-group">
@@ -23,7 +23,6 @@
 					<i class="material-icons accent">expand_more</i>
 				</a>
 				</h5>
-				<p>sample</p>
 			</div>
 		</div>
 	</div>
