d19e1da830884e95f8674186dd43512b0e97f27e
shawrlot <charlotteramosvalenzuela@gmail.com>
2017-11-04 17:35:09 +0800

    Revert "Revert "add announcement/event""
    This reverts commit 5bdca960494f5fb5af419c0d117be87cbcbf50ce.

--- application/controllers/Authentication.php
+++ application/controllers/Authentication.php
@@ -3,25 +3,27 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Authentication extends CI_Controller {
 
-	public function login(){
-		if ($this->input->post()){
+public function login(){
+	if($this->session->userdata('logged_in')){
+		redirect('home');
+		} else {
+			if ($this->input->post()){
 			$data = array(
-				'account_username'=>$this->input->post('username'),
-				'account_password'=>$this->input->post('password'),
+			'account_username'=>$this->input->post('username'),
+			'account_password'=>$this->input->post('password'),
 			);
 			$this->load->model('authentication_model');
-
 			if ($this->authentication_model->login($data)) {
-				header('location:/');
-			}
-			else{
-				header('location:/');
-			}
-		}
-		else{
+			header('location:/');
+			} else {
+			header('location:/');
+			 }
+		} else {
 			$this->load->view('login_view');
 		}
 	}
+}
+
 	public function logout(){
 		$this->session->unset_userdata('logged_in');
 		header('location:/');
